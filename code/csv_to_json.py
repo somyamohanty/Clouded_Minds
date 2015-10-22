afeaa6e9d25244b1f33269c2f8a748a117f9ab12
PsiLupan <wwc77@msstate.edu>
2015-10-22 15:44:45 -0500

    Added support for the "lang" field available in our new data, in order to check for 'en' and 'es'. This is a result of Sentiment140 only supporting those two langauges.

--- code/csv_to_json.py
+++ code/csv_to_json.py
@@ -5,6 +5,7 @@ inname = "../data/arsliv.csv"
 outname = "../data/arsliv.json"
 
 infile = open(inname, 'r')
+lang_avail = False #This field indicates if this CSV has the lang column, currently manual entry
 outfile = open(outname, 'w')
 
 linenum = 0
@@ -13,10 +14,19 @@ for line in infile:
 	if linenum == 0: #Ignore the column headers
 		linenum += 1
 		continue
-	if linenum > 1:
-		outfile.write(',') #Writes the closing comma of {"text": "I love Titanic.", "id": 1234}, 
-	text = line.split(',')[0]
-	outfile.write("{\"text\": \""+text+ "\", \"id\": "+str(linenum)+"}")
+
+	line_split = line.split(',')
+	text = line_split[0]
+	if lang_avail == True:
+		lang = line_split[5]
+		if lang.startswith('en') or lang.startswith('es'): #Sentiment140 only supports these two. They can be en-US, etc. so we check the first part
+			if linenum > 1:
+				outfile.write(',') #Writes the closing comma of {"text": "I love Titanic.", "id": 1234}, 
+			outfile.write("{\"text\": \""+text+ "\", \"id\": "+str(linenum)+"}")
+	else:
+		if linenum > 1:
+			outfile.write(',')
+		outfile.write("{\"text\": \""+text+ "\", \"id\": "+str(linenum)+"}")
 	linenum += 1
 
 infile.close()
