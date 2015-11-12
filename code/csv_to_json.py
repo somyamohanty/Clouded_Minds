26d9e322a26849c23bde59be4b4a37b1d076f98a
PsiLupan <wwc77@msstate.edu>
2015-11-12 16:07:38 -0600

    Change csv_to_json to split file to 1500 tweets per file.

--- code/csv_to_json.py
+++ code/csv_to_json.py
@@ -1,33 +1,52 @@
 #Converts the CSV format to the Sentiment140 compatible json
 #Note that it would be better to use the MongoDB directly
 
-inname = "../data/arsliv.csv"
-outname = "../data/arsliv.json"
+gamename = "chears"
+
+inname = "../data/" + gamename + ".csv"
+outname = "../data/json/" +gamename + "/orig/" + gamename
+
+file_num = 1
 
 infile = open(inname, 'r')
 lang_avail = False #This field indicates if this CSV has the lang column, currently manual entry
-outfile = open(outname, 'w')
+outfile = open(outname + '_' + str(file_num) + '.json', 'w')
 
 linenum = 0
+act_linenum = 0
+
 outfile.write('{"language": "auto", "data": [')
 for line in infile:
 	if linenum == 0: #Ignore the column headers
 		linenum += 1
 		continue
 
+	if(act_linenum == 1500): #TODO: Get an actual limit from the API
+		outfile.write("]}")
+		outfile.close()
+		file_num += 1
+		outfile = open(outname + '_' + str(file_num) + '.json', 'w')
+		outfile.write('{"language": "auto", "data": [')
+		act_linenum = 0
+
 	line_split = line.split(',')
 	text = line_split[0].strip().translate(None,'"\/\\\r\t\n\b\f\t')
+		
 	if len(text) > 3: #Prevent empty text field ("text": "")
 		if lang_avail == True:
 			lang = line_split[5]
-			if lang.startswith('en') or lang.startswith('es'): #Sentiment140 only supports these two. They can be en-US, etc. so we check the first part
+			if lang.startswith('en') or lang.startswith('es'): #Sentiment140 only supports these two langs
 				if linenum > 1:
 					outfile.write(',') #Writes the closing comma of {"text": "I love Titanic.", "id": 1234}, 
+				
 				outfile.write('{"text": "'+ text + '", "id": '+ __builtins__.str(linenum) +'}')
+				act_linenum += 1
 		else:
 			if linenum > 1:
 				outfile.write(',')
 			outfile.write('{"text": "'+ text +'", "id": '+ __builtins__.str(linenum) +'}')
+			act_linenum += 1
+	
 	linenum += 1
 
 infile.close()
