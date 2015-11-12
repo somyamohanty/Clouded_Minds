7660e0b4cf41d1222f613b8bceaae86821fcbbe5
PsiLupan <wwc77@msstate.edu>
2015-11-12 16:08:20 -0600

    Updated to proper API key and changed directory structure. Directories must currently be manually built for sentiment analysis.

--- code/sentiment.py
+++ code/sentiment.py
@@ -1,13 +1,15 @@
 import requests
 import json
 
-json_file = open("../data/arsliv.json").read() #TODO: Switch to a for loop to populate tweet_data, since request limit may be less than a file
-tweet_data = json.loads(json_file)
+gamename = 'chears'
 
-url = "http://www.sentiment140.com/api/bulkClassifyJson?appid=wwc77@msstate.edu"
+json_data = open("../data/json/" + gamename + "/orig/" + gamename + "_1.json").read() #TODO: For loop through each file in a directory, use the filename as a variable, and provide it to the output filename
+tweet_data = json.loads(json_data)
+
+url = "http://www.sentiment140.com/api/bulkClassifyJson?appid=wcarter312@gmail.com"
 tweet_req = requests.post(url, json=tweet_data)
 if tweet_req.status_code == 200:
-	senti_out = open("../data/arsliv_sentiment.json", 'w')
+	senti_out = open("../data/json/"+gamename +"/senti/"+ gamename +"_1.json", 'w')
 	for line in tweet_req.iter_lines():
 		senti_out.write(line)
 else:
