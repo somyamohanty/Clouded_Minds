b92211ed25206c604b52b76e61ec4ab0f0c7f865
Michael <msn42@msstate.edu>
2015-11-15 15:40:22 -0600

    Added sentiment analysis on multiple files.

--- code/sentiment.py
+++ code/sentiment.py
@@ -1,16 +1,24 @@
 import requests
 import json
 
-gamename = 'chears'
-
-json_data = open("../data/json/" + gamename + "/orig/" + gamename + "_1.json").read() #TODO: For loop through each file in a directory, use the filename as a variable, and provide it to the output filename
-tweet_data = json.loads(json_data)
-
 url = "http://www.sentiment140.com/api/bulkClassifyJson?appid=wcarter312@gmail.com"
-tweet_req = requests.post(url, json=tweet_data)
-if tweet_req.status_code == 200:
-	senti_out = open("../data/json/"+gamename +"/senti/"+ gamename +"_1.json", 'w')
-	for line in tweet_req.iter_lines():
-		senti_out.write(line)
-else:
-	print "ERROR: " + tweet_req.status_code
+gamename = 'chears'
+fn = 1
+while fn != 0:
+    try:
+        json_data = open("../data/json/" + gamename + "/orig/" + gamename + "_" + str(fn) + ".json").read()
+    except IOError:
+        print "Break" + str(fn)
+        break
+    tweet_data = json.loads(json_data)
+    tweet_req = requests.post(url, json=tweet_data)
+    if tweet_req.status_code == 200:
+        senti_out = open("../data/json/"+gamename +"/senti/"+ gamename +"_" + str(fn) + ".json", 'w')
+        for line in tweet_req.iter_lines():
+            senti_out.write(line)         
+    else:
+        print "ERROR: " + tweet_req.status_code
+        break   
+    print fn
+    fn = fn + 1  
+print "Finished"
