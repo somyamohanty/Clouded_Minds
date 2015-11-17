1068bfb4cdb7feb4c55cbcc403795a225677bb32
Sandra Gibson <slg233@msstate.edu>
2015-11-17 14:36:50 -0600

    sentiment and csv to json with for loops added.  Needs to be tested.

---
+++ code/sentiment_loop.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+url = "http://www.sentiment140.com/api/bulkClassifyJson?appid=wcarter312@gmail.com"
+gamenames = {"chears", "livnor", "mcinew", "soumun", "swaeve"}
+for gamename in gamenames:
+    
+    fn = 1
+    while fn != 0:
+        try:
+            json_data = open("../data/json/" + gamename + "/orig/" + gamename + "_" + str(fn) + ".json").read()
+        except IOError:
+            print "Break" + str(fn)
+            break
+        tweet_data = json.loads(json_data)
+        tweet_req = requests.post(url, json=tweet_data)
+        if tweet_req.status_code == 200:
+            senti_out = open("../data/json/"+gamename +"/senti/"+ gamename +"_" + str(fn) + ".json", 'w')
+            for line in tweet_req.iter_lines():
+                senti_out.write(line)         
+        else:
+            print "ERROR: " + tweet_req.status_code
+            break   
+        print fn
+        fn = fn + 1  
+    print "Finished"
