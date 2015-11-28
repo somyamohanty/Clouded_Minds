8a877d5bd0ccd951f0bf920e155584699b29aca6
Michael <msn42@msstate.edu>
2015-11-27 22:22:39 -0600

    Updated to take the input file as a CSV. This allows for better text input for sentiment analysis and ID. Originally it would sometimes split 1 tweet into multiple different tweets because it was on different lines. This fixes that problem.

--- code/csv_to_json.py
+++ code/csv_to_json.py
@@ -1,55 +1,50 @@
 #Converts the CSV format to the Sentiment140 compatible json
 #Note that it would be better to use the MongoDB directly
 
+#Config variables
 gamename = "chears"
 
-inname = "../data/" + gamename + ".csv"
+#Output File and Input File
 outname = "../data/json/" +gamename + "/orig/" + gamename
-
-file_num = 1
-
-infile = open(inname, 'r')
-lang_avail = False #This field indicates if this CSV has the lang column, currently manual entry
+reader = csv.DictReader(open("../data/" + gamename + ".csv", 'rb'), delimiter=',', quotechar='"')
 outfile = open(outname + '_' + str(file_num) + '.json', 'w')
 
-linenum = 0
+#Variables to operate
+file_num = 1
+lang_avail = False #This field indicates if this CSV has the lang column, currently manual entry
+linenum = 2
 act_linenum = 0
 
 outfile.write('{"language": "auto", "data": [')
-for line in infile:
-	if linenum == 0: #Ignore the column headers
-		linenum += 1
-		continue
+for row in reader:
+    if(act_linenum == 5000): #5000 is based on API's recommendation for 60 second timeout window
+        outfile.write("]}")
+        outfile.close()
+        file_num += 1
+        outfile = open(outname + '_' + str(file_num) + '.json', 'w')
+        outfile.write('{"language": "auto", "data": [')
+        act_linenum = 0
 
-	if(act_linenum == 5000): #5000 is based on API's recommendation for 60 second timeout window
-		outfile.write("]}")
-		outfile.close()
-		file_num += 1
-		outfile = open(outname + '_' + str(file_num) + '.json', 'w')
-		outfile.write('{"language": "auto", "data": [')
-		act_linenum = 0
-
-	line_split = line.split(',')
-	text = line_split[0].strip().translate(None,'"\/\\\r\t\n\b\f\t')
-		
-	if len(text) > 3: #Prevent empty text field ("text": "")
-		if lang_avail == True:
-			lang = line_split[5]
-			if lang.startswith('en') or lang.startswith('es'): #Sentiment140 only supports these two langs
-				if linenum > 1  and act_linenum != 0:
-					outfile.write(',') #Writes the closing comma of {"text": "I love Titanic.", "id": 1234}, 
-				
-				outfile.write('{"text": "'+ text + '", "id": '+ __builtins__.str(linenum) +'}')
-				act_linenum += 1
-		else:
-			if linenum > 1  and act_linenum != 0:
-				outfile.write(',')
-			outfile.write('{"text": "'+ text +'", "id": '+ __builtins__.str(linenum) +'}')
-			act_linenum += 1
-	
-	linenum += 1
+    cleantext = row['text'].strip().translate(None,'"\/\\\r\t\n\b\f\t')
+    
+    if len(cleantext) > 3: #Prevent empty text field ("text": "")
+        if lang_avail == True:
+            lang = line_split[5]
+            if lang.startswith('en') or lang.startswith('es'): #Sentiment140 only supports these two langs
+                if linenum > 1 and act_linenum != 0:
+                    outfile.write(',') #Writes the closing comma of {"text": "I love Titanic.", "id": 1234}, 
+                
+                outfile.write('{"text": "'+ cleantext + '", "id": '+ __builtins__.str(linenum) +'}')
+                act_linenum += 1
+        else:
+            if linenum > 1 and act_linenum != 0:
+                outfile.write(',')
+            outfile.write('{"text": "'+ cleantext +'", "id": '+ __builtins__.str(linenum) +'}')
+            act_linenum += 1
+    
+    linenum += 1
 
 infile.close()
 outfile.write("]}")
 outfile.close()
-	
+    
