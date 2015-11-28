e1d82f3e3e9025907a15e4ffc858f2c7491080b6
Michael <msn42@msstate.edu>
2015-11-27 22:16:32 -0600

    Updated sentiment analysis to only go to the number of files in the directory instead of throwing an error to get out of the loop. Still a bug in the last file that is retrieved.

--- code/sentiment.py
+++ code/sentiment.py
@@ -1,14 +1,18 @@
 import requests
 import json
+import os
 
 url = "http://www.sentiment140.com/api/bulkClassifyJson?appid=wcarter312@gmail.com"
 gamename = 'chears'
 fn = 1
-while fn != 0:
+path, dirs, files = os.walk("../data/json/" + gamename + "/senti/").next()
+max_files = len(files) - 1 #Mac creates invisible file for - 1
+
+while fn <= max_files:
     try:
         json_data = open("../data/json/" + gamename + "/orig/" + gamename + "_" + str(fn) + ".json").read()
     except IOError:
-        print "Break" + str(fn)
+        print "Error opening file: " + str(fn)
         break
     tweet_data = json.loads(json_data)
     tweet_req = requests.post(url, json=tweet_data)
