2f7137ccfcccfd8f7309bdbb9bc17b47b8fb8e97
Michael Nichols <howdoom@hotmail.com>
2015-12-02 03:15:31 -0600

    Merged changes with other csv_to_json file and added support to read language column.

--- code/csv_to_json_loop.py
+++ code/csv_to_json_loop.py
@@ -1,18 +1,17 @@
-#Converts the CSV format to the Sentiment140 compatible json
-#Note that it would be better to use the MongoDB directly
-import os, sys
-
 #gamenames = {"chears", "livnor", "mcinew", "soumun", "swaeve"} #all game names
+#converts the CSV format to the Sentiment140 compatible json
+import csv, sys, os
+
 gamenames = {"mcinew"} #lang tagged games
 for gamename in gamenames:
-
+    
+    lang_avail = True #This field indicates if this CSV has the lang column, currently manual entry
+    file_num = 1
     inname = "../data/" + gamename + ".csv"
     outname = "../data/json/" +gamename + "/orig/" + gamename
-
-    file_num = 1
-
-    infile = open(inname, 'r')
-    lang_avail = True #This field indicates if this CSV has the lang column, currently manual entry
+    reader = csv.DictReader(open(inname), delimiter=',', quotechar='"')
+    
+    
     if not os.path.exists("../data/json/"+gamename+"/orig/"):
 	os.makedirs("../data/json/"+gamename+"/orig/")
     outfile = open(outname + '_' + str(file_num) + '.json', 'w')
@@ -21,40 +20,34 @@ for gamename in gamenames:
     act_linenum = 0
 
     outfile.write('{"language": "auto", "data": [')
-    for line in infile:
-        if linenum == 0: #Ignore the column headers
-            linenum += 1
-            continue
+for row in reader:
+    if(act_linenum == 5000): #5000 is based on API's recommendation for 60 second timeout window
+        outfile.write("]}")
+        outfile.close()
+        file_num += 1
+        outfile = open(outname + '_' + str(file_num) + '.json', 'w')
+        outfile.write('{"language": "auto", "data": [')
+        act_linenum = 0
 
-        if(act_linenum == 5000): #5000 is based on API's recommendation for 60 second timeout window
-            outfile.write("]}")
-            outfile.close()
-            file_num += 1
-            outfile = open(outname + '_' + str(file_num) + '.json', 'w')
-            outfile.write('{"language": "auto", "data": [')
-            act_linenum = 0
-
-        line_split = line.split(',')
-        text = line_split[0].strip().translate(None,'"\/\\\r\t\n\b\f\t')
-
-        if len(text) > 3: #Prevent empty text field ("text": "")
-            if lang_avail == True:
-                lang = line_split[5]
-                if lang.startswith('en') or lang.startswith('es'): #Sentiment140 only supports these two langs
-                    if linenum > 1  and act_linenum != 0:
-                        outfile.write(',') #Writes the closing comma of {"text": "I love Titanic.", "id": 1234}, 
+    cleantext = row['text'].strip().translate(None,'"\/\\\r\t\n\b\f\t')
+    clearlang = row['lang'].strip().translate(None,'"\/\\\r\t\n\b\f\t')
+    
+    if len(cleantext) > 3: #Prevent empty text field ("text": "")
+        if lang_avail == True:
+            if clearlang.startswith('en') or clearlang.startswith('es'): #Sentiment140 only supports these two langs
+                if linenum > 1 and act_linenum != 0:
+                    outfile.write(',') #Writes the closing comma of {"text": "I love Titanic.", "id": 1234}, 
                     
-                    outfile.write('{"text": "'+ text + '", "id": '+ __builtins__.str(linenum) +'}')
-                    act_linenum += 1
-            else:
-                if linenum > 1  and act_linenum != 0:
-                    outfile.write(',')
-                outfile.write('{"text": "'+ text +'", "id": '+ __builtins__.str(linenum) +'}')
+                outfile.write('{"text": "'+ cleantext + '", "id": '+ __builtins__.str(linenum) +'}')
                 act_linenum += 1
-        
-        linenum += 1
+        else:
+            if linenum > 1 and act_linenum != 0:
+                outfile.write(',')
+            outfile.write('{"text": "'+ cleantext +'", "id": '+ __builtins__.str(linenum) +'}')
+            act_linenum += 1
+    
+    linenum += 1
 
-    infile.close()
-    outfile.write("]}")
-    outfile.close()
-	
+outfile.write("]}")
+outfile.close()
+    
