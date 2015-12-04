1f4c7511cb623acb2c57ad17217d8e6697ee3e93
Michael Nichols <howdoom@hotmail.com>
2015-12-04 13:59:49 -0600

    Change: Add more config options to make it more easily customizable to new games. Added sentinel analysis split for manchester and newcastle.

--- code/split_teams.py
+++ code/split_teams.py
@@ -4,9 +4,11 @@ import csv
 import json
 
 #Config
-gamename = 'chears'
-t1 = "Arsenal" #AFC
-t2 = "Chelsea" #CFC
+gamename = 'mcinew'
+t1 = "Manchester"
+t1_hasht = "#MCFC"
+t2 = "Newcastle"
+t2_hasht = "#NUFC"
 
 #Open Game
 reader = csv.DictReader(open("../data/" + gamename + "_sent.csv", 'rb'), delimiter=',', quotechar='"')
@@ -14,7 +16,7 @@ reader = csv.DictReader(open("../data/" + gamename + "_sent.csv", 'rb'), delimit
 #Write game split into teams
 with open("../data/" + gamename + "_sent_" +t1 + ".csv", 'w') as team1_f:
     with open("../data/" + gamename + "_sent_" + t2 + ".csv", 'w') as team2_f:
-        fieldnames = ['text', 'user', 'created_at','timestamp', 'geo','polarity']
+        fieldnames = ['text', 'user', 'created_at','timestamp', 'geo', 'lang','polarity']
         t1_writer = csv.DictWriter(team1_f, delimiter=',', fieldnames=fieldnames)
         t1_writer.writeheader()
         t2_writer = csv.DictWriter(team2_f, delimiter=',', fieldnames=fieldnames)
@@ -22,25 +24,26 @@ with open("../data/" + gamename + "_sent_" +t1 + ".csv", 'w') as team1_f:
 
         for row in reader:
             #Team 1
-                if row['text'].find("#AFC") >= 0:
+                if row['text'].find(t1_hasht) >= 0 or row['text'].find(t1) >= 0:
                     t1_writer.writerow({
                                 'text': row['text'],
                                 'user': row['user'],
                                 'created_at': row['created_at'],
                                 'timestamp': row['timestamp'],
                                 'geo': row['geo'],
+                                'lang' : row['lang'],
                                 'polarity' : row['polarity'],
                             })
                 #Team 2
-                if row['text'].find("#CFC") >= 0: 
+                if row['text'].find(t2_hasht) >= 0 or row['text'].find(t2) >= 0: 
                      t2_writer.writerow({
                                 'text': row['text'],
                                 'user': row['user'],
                                 'created_at': row['created_at'],
                                 'timestamp': row['timestamp'],
                                 'geo': row['geo'],
+                                'lang' : row['lang'],
                                 'polarity' : row['polarity'],
                             })
 team1_f.close()
 team2_f.close()
-reader.close()
