92141e2e141e8eb9f8d5fcec8b28617d92617091
PsiLupan <wwc77@msstate.edu>
2015-12-06 13:43:49 -0600

    Updated sentiment loop to support new dataset names.

--- code/sentiment_loop.py
+++ code/sentiment_loop.py
@@ -3,18 +3,14 @@ import json
 import os, sys
 
 url = "http://www.sentiment140.com/api/bulkClassifyJson?appid=wcarter312@gmail.com"
-gamenames = {"chears", "livnor", "mcinew", "soumun", "swaeve"}
+gamenames = {"engfra", "mciliv", "realbarca", "swabou"}
 for gamename in gamenames:
     if not os.path.exists("../data/json/"+gamename+"/senti/"):
 	os.makedirs("../data/json/"+gamename+"/senti/")    
 
     fn = 1
     while fn != 0:
-        try:
-            json_data = open("../data/json/" + gamename + "/orig/" + gamename + "_" + str(fn) + ".json").read()
-        except IOError:
-            print "Break" + str(fn)
-            break
+        json_data = open("../data/json/" + gamename + "/orig/" + gamename + "_" + str(fn) + ".json").read()
         tweet_data = json.loads(json_data)
         tweet_req = requests.post(url, json=tweet_data)
         if tweet_req.status_code == 200:
